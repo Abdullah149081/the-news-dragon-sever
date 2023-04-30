4ae40cbb1391323927e624615434db542f1925b1
Abdullah149081 <masudmd425@gmail.com>
2023-04-30 12:22:56 +0600

    first commit

---
+++ __step.js
@@ -0,0 +1,19 @@
+/* 
+1. mkdir folder name.
+2. cd folder
+3. npm init -y
+4. yarn add express
+5. create index.js file = > go to (https://expressjs.com/en/starter/hello-world.html)
+
+6. go to (https://expressjs.com/en/resources/middleware/cors.html)
+
+7.  yarn add cors
+8. add index.js =>  const cors = require('cors') / app.use(cors())
+
+
+client site 
+------------
+const [categories, setCategories] = useState([]);
+useEffect add 
+fetch("http://localhost:5000/categories")
+*/
