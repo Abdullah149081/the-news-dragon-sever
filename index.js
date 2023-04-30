8d2ef4b4ee77af1d9b9f550c3732f18896d79c8d
Abdullah149081 <masudmd425@gmail.com>
2023-04-30 18:34:10 +0600

    done

--- index.js
+++ index.js
@@ -11,22 +11,23 @@ app.get("/", (req, res) => {
   res.send("Dragon is running");
 });
 
+// all categories load
 app.get("/categories", (req, res) => {
   res.send(categories);
 });
-
+// all news load
 app.get("/news", (req, res) => {
   res.send(news);
 });
 
-//important
+//important 1 news load
 app.get("/news/:id", (req, res) => {
   const id = req.params.id;
   const selectedNews = news.find((n) => n._id === id);
   res.send(selectedNews);
 });
 
-// important
+// important categories way news load
 app.get("/categories/:id", (req, res) => {
   const id = req.params.id;
 
